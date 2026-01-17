878d359add0575387df1c6f0295b702137bca20c
bakernigel <bakernigel@yahoo.com>
2026-01-17 11:57:29 -0700

    Fix attr COLOR_TEMP

--- custom_components/smartthings/light.py
+++ custom_components/smartthings/light.py
@@ -11,7 +11,7 @@ from pysmartthings import Capability
 
 from homeassistant.components.light import (
     ATTR_BRIGHTNESS,
-    ATTR_COLOR_TEMP,
+    ATTR_COLOR_TEMP_KELVIN,
     ATTR_HS_COLOR,
     ATTR_TRANSITION,
     ColorMode,
